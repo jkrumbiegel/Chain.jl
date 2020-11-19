13be3542931c8f63383ff60cb08fb1c1a2dd4509
jkrumbiegel <22495855+jkrumbiegel@users.noreply.github.com>
2020-11-19 15:47:14 +0100

    initial

--- src/PipelessPipes.jl
+++ src/PipelessPipes.jl
@@ -1,5 +1,66 @@
 module PipelessPipes
 
-# Write your package code here.
+export @_
+
+function get_unblocked_parts(x, exprs = Any[])
+    if x isa Expr && x.head == :block
+        for arg in x.args
+            exprs = get_unblocked_parts(arg, exprs)
+        end
+    else
+        push!(exprs, x)
+    end
+    exprs
+end
+
+macro _(df, block)
+    if !(block isa Expr && block.head == :block)
+        error("Second argument must be a begin / end block")
+    end
+
+    unblocked_parts = get_unblocked_parts(block)
+
+    newexprs = []
+    lastsym = df
+    for part in unblocked_parts
+        if part isa LineNumberNode
+            push!(newexprs, part)
+            continue
+        end
+        need_new_variable = false
+        newexpr = postwalk(part) do expr
+            if expr == Symbol("_")
+                need_new_variable = true
+                lastsym
+            elseif expr == Symbol("__")
+                lastsym
+            else
+                expr
+            end
+        end
+        if need_new_variable
+            newsym = gensym()
+            push!(newexprs, Expr(Symbol("="), newsym, newexpr))
+            lastsym = newsym
+        else
+            push!(newexprs, newexpr)
+        end
+    end
+    result = Expr(:let, Expr(:block), Expr(:block, newexprs...))
+
+    :($(esc(result)))
+end
+
+## copied from MacroTools in order to avoid dependency
+
+walk(x, inner, outer) = outer(x)
+walk(x::Expr, inner, outer) = outer(Expr(x.head, map(inner, x.args)...))
+
+"""
+    postwalk(f, expr)
+Applies `f` to each node in the given expression tree, returning the result.
+`f` sees expressions *after* they have been transformed by the walk.
+"""
+postwalk(f, x) = walk(x, x -> postwalk(f, x), f)
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,22 @@ using PipelessPipes
 using Test
 
 @testset "PipelessPipes.jl" begin
-    # Write your tests here.
+    x = [1, 2, 3]
+    y = @_ x begin
+        sum(_)
+    end
+    @test y == sum(x)
+
+    z = @_ x begin
+        _ .* 3
+        sum(_)
+    end
+    @test z == sum(x .* 3)
+
+    zz = @_ x begin
+        _ .* 3
+        @assert sum(__) / length(__) == 6 # this doesn't change anything
+        sum(_)
+    end
+    @test zz == z
 end
