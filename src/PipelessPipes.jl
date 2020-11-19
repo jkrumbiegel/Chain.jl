b3ea845fe4bb320ffe2211af71b25704a9b502c9
jkrumbiegel <22495855+jkrumbiegel@users.noreply.github.com>
2020-11-19 15:16:04 +0100

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.13

---
+++ src/PipelessPipes.jl
@@ -0,0 +1,5 @@
+module PipelessPipes
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using PipelessPipes
+using Test
+
+@testset "PipelessPipes.jl" begin
+    # Write your tests here.
+end
