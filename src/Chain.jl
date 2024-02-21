3e3097545f76763ca54c87472db71088f2b1c473
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2024-02-21 09:48:52 +0100

    flatten begin blocks anywhere in the chain

--- src/Chain.jl
+++ src/Chain.jl
@@ -127,57 +127,130 @@ function rewrite_chain_block(firstpart, block)
 end
 
 """
-    @chain(initial_value, block::Expr)
+    @chain(expr, exprs...)
 
-Rewrites a block expression to feed the result of each line into the next one.
-The initial value is given by the first argument.
+Rewrites a series of expressions into a chain, where the result of one expression
+is inserted into the next expression following certain rules.
 
-In all lines, underscores are replaced by the previous line's result.
-If there are no underscores and the expression is a symbol, the symbol is rewritten
-to a function call with the previous result as the only argument.
-If there are no underscores and the expression is a function call or a macrocall,
-the call has the previous result prepended as the first argument.
+**Rule 1**
 
-Example:
+Any `expr` that is a `begin ... end` block is flattened.
+For example, these two pseudocodes are equivalent:
 
+```julia
+@chain a b c d e f
+
+@chain a begin
+    b
+    c
+    d
+end e f
 ```
-x = @chain [1, 2, 3] begin
-    filter(!=(2), _)
-    sqrt.(_)
+
+**Rule 2**
+
+Any expression but the first (in the flattened representation) will have the preceding result
+inserted as its first argument, unless at least one underscore `_` is present.
+In that case, all underscores will be replaced with the preceding result.
+
+If the expression is a symbol, the symbol is treated equivalently to a function call.
+
+For example, the following code block
+
+```julia
+@chain begin
+    x
+    f()
+    @g()
+    h
+    @i
+    j(123, _)
+    k(_, 123, _)
+end
+```
+
+is equivalent to
+
+```julia
+begin
+    local temp1 = f(x)
+    local temp2 = @g(temp1)
+    local temp3 = h(temp2)
+    local temp4 = @i(temp3)
+    local temp5 = j(123, temp4)
+    local temp6 = k(temp5, 123, temp5)
+end
+```
+
+**Rule 3**
+
+An expression that begins with `@aside` does not pass its result on to the following expression.
+Instead, the result of the previous expression will be passed on.
+This is meant for inspecting the state of the chain.
+The expression within `@aside` will not get the previous result auto-inserted, you can use
+underscores to reference it.
+
+```julia
+@chain begin
+    [1, 2, 3]
+    filter(isodd, _)
+    @aside @info "There are \$(length(_)) elements after filtering"
     sum
 end
-x == sum(sqrt.(filter(!=(2), [1, 2, 3])))
 ```
-"""
-macro chain(initial_value, block::Expr)
-    if !(block.head == :block)
-        block = Expr(:block, block)
-    end
-    rewrite_chain_block(initial_value, block)
+
+**Rule 4**
+
+It is allowed to start an expression with a variable assignment.
+In this case, the usual insertion rules apply to the right-hand side of that assignment.
+This can be used to store intermediate results.
+
+```julia
+@chain begin
+    [1, 2, 3]
+    filtered = filter(isodd, _)
+    sum
 end
 
-"""
-    @chain(initial_value, args...)
-
-Rewrites a series of argments, either expressions or symbols, to feed the result
-of each line into the next one. The initial value is given by the first argument.
-
-In all arguments, underscores are replaced by the argument's result.
-If there are no underscores and the argument is a symbol, the symbol is rewritten
-to a function call with the previous result as the only argument.
-If there are no underscores and the argument is a function call or a macrocall,
-the call has the previous result prepended as the first argument.
-
-Example:
-
+filtered == [1, 3]
 ```
-x = @chain [1, 2, 3] filter(!=(2), _) sqrt.(_) sum
 
-x == sum(sqrt.(filter(!=(2), [1, 2, 3])))
+**Rule 5**
+
+The `@.` macro may be used with a symbol to broadcast that function over the preceding result.
+
+```julia
+@chain begin
+    [1, 2, 3]
+    @. sqrt
+end
 ```
+
+is equivalent to
+
+```julia
+@chain begin
+    [1, 2, 3]
+    sqrt.(_)
+end
+```
+
 """
 macro chain(initial_value, args...)
-    rewrite_chain_block(initial_value, Expr(:block, args...))
+    block = flatten_to_single_block(initial_value, args...)
+    rewrite_chain_block(block)
+end
+
+function flatten_to_single_block(args...)
+    blockargs = []
+    for arg in args
+        if arg isa Expr && arg.head === :block
+            append!(blockargs, arg.args)
+        else
+            push!(blockargs, arg)
+        end
+    end
+    Expr(:block, blockargs...)
 end
 
 function rewrite_chain_block(block)
@@ -232,34 +305,6 @@ function reconvert_docstrings!(args::Vector)
         splice!(args, i:i, [str, nextline])
     end
     args
-end
-
-"""
-    @chain(block::Expr)
-
-Rewrites a block expression to feed the result of each line into the next one.
-The first line serves as the initial value and is not rewritten.
-
-In all other lines, underscores are replaced by the previous line's result.
-If there are no underscores and the expression is a symbol, the symbol is rewritten
-to a function call with the previous result as the only argument.
-If there are no underscores and the expression is a function call or a macrocall,
-the call has the previous result prepended as the first argument.
-
-Example:
-
-```
-x = @chain begin
-    [1, 2, 3]
-    filter(!=(2), _)
-    sqrt.(_)
-    sum
-end
-x == sum(sqrt.(filter(!=(2), [1, 2, 3])))
-```
-"""
-macro chain(block::Expr)
-    rewrite_chain_block(block)
 end
 
 function replace_underscores(expr::Expr, replacement)
